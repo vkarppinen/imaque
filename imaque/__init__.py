293643eb5e52f6eed446d83e672291ee18131c54
vkarppinen <karppinenvaltteri@gmail.com>
2018-03-03 16:19:25 +0200

    configure basicc routes

--- imaque/__init__.py
+++ imaque/__init__.py
@@ -1,4 +1,5 @@
 from pyramid.config import Configurator
+from imaque.views import handle_upload
 
 
 def main(global_config, **settings):
@@ -7,6 +8,11 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('hello', '/')
+
+    config.add_route('form', '/')
+
+    config.add_route('upload', '/upload')
+    config.add_view(handle_upload, route_name='upload')
+
     config.scan()
     return config.make_wsgi_app()
