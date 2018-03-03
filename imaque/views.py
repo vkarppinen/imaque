079a1325ffce3b7f812734364558a51ea2d1a11a
vkarppinen <karppinenvaltteri@gmail.com>
2018-03-03 16:20:22 +0200

    handle file upload

--- imaque/views.py
+++ imaque/views.py
@@ -1,14 +1,20 @@
+from base64 import b64encode
+
 from pyramid.response import Response
 from pyramid.view import view_config
 
+@view_config(route_name='form', renderer='templates/template.jinja2')
+def form(request):
+    return dict()
 
-# @view_config(route_name='home', renderer='templates/mytemplate.jinja2')
-# def my_view(request):
-#     return {'project': 'imaque'}
 
-@view_config(route_name='hello')
-def hello(request):
+def handle_upload(request):
+    try:
+        image = request.POST['image']
+        b64image = b64encode(image.file.read())
+    except:
+        return Response('No image was sent!')
+
     return Response(
-        '<h1>Hello world</h1>'
+        'not implemented yet'
     )
-
